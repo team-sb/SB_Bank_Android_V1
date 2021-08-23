7bce7a8af6d18cd0c04556f772ecb0a0678c656e
limsaehyun <sh00710000@gmail.com>
2021-08-23 23:12:20 +0900

    Feat :: 잔액 조회 Response

---
+++ app/src/main/java/com/example/bank/MainBalanceResponse.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+public class MainBalanceResponse {
+    String balance;
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+}
