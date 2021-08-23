cef4d854b4880a43b0dcb860d295b68f0119bc2f
limsaehyun <sh00710000@gmail.com>
2021-08-23 23:12:07 +0900

    Feat :: 로그인 Response

---
+++ app/src/main/java/com/example/bank/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+public class LoginResponse {
+    String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
