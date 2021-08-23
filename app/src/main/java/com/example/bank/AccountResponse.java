5730c4f1b0a2edb7e941c2135c7f5c175da0a574
limsaehyun <sh00710000@gmail.com>
2021-08-23 23:11:50 +0900

    Feat :: 계좌 Response

---
+++ app/src/main/java/com/example/bank/AccountResponse.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+public class AccountResponse {
+    String account; // 계좌 번호
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+}
