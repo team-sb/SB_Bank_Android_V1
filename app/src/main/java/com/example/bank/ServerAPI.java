90273f7d8ae1fa823a2d12483312a7fea93ca52b
limsaehyun <sh00710000@gmail.com>
2021-08-23 23:14:33 +0900

    Feat :: API 인터페이스

---
+++ app/src/main/java/com/example/bank/ServerAPI.java
@@ -0,0 +1,55 @@
+package com.example.bank;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface ServerAPI {
+
+    // Auth
+
+    @POST("login") // 로그인
+    Call<LoginResponse> Login(@Body LoginRequest loginRequest);
+
+    @POST("join") // 회원가입
+    Call<RegisterResponse> Register(@Body RegisterRequest registerRequest);
+
+    // Account
+
+    @POST("account/register") // 계좌 생성
+    Call<AccountRequest> registerAccount(@Header("Authorization") String token, @Body String sec_password);
+
+    @POST("account/charge") // 입출금
+    Call<AccountRequest> chargeAccount(@Header("Authorization") String token, @Body String sec_password, @Body String money);
+
+    @POST("account/transfer") // 계좌이체
+    Call<AccountRequest> transferAccount(@Header("Authorization") String token, @Body String sec_password, @Body String money, @Body String target);
+
+    @POST("account/borrow") // 대출
+    Call<AccountRequest> borrowAccount(@Header("Authorization") String token, @Body String sec_password, @Body String money);
+
+    // MainPage
+
+    @GET("user/balance/{user-id}") // 잔액 표시
+    Call<MainPageRequest> balanceUser(@Header("Authorization") String token, @Path("user-id") int user_id);
+
+    @GET("user/loan/{user-id}") // 대출 현황
+    Call<MainPageRequest> loanUser(@Header("Authorization") String token, @Path("user-id") int user_id);
+
+    @GET("user/credit/{user-id}") // 신용 등급
+    Call<MainPageRequest> creditUser(@Header("Authorization") String token, @Path("user-id") int user_id);
+
+    @GET("user/transaction/{user-id}") // 거래 내역(전체)
+    Call<MainPageRequest> transactionUser(@Header("Authorization") String token, @Path("user-id") int user_id);
+
+    @GET("user/transaction/send/{user-id}") // 거래 내역(입금)
+    Call<MainPageRequest> transactionSendUser(@Header("Authorization") String token, @Path("user-id") int user_id);
+
+    @GET("user/transaction/receive/{user-id}") // 거래 내역(출금)
+    Call<MainPageRequest> transactionReceiveUser(@Header("Authorization") String token, @Path("user-id") int user_id);
+}
