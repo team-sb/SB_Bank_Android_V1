84f1171126b9bf9247e3b705b6285457860d50df
limsaehyun <sh00710000@gmail.com>
2021-08-23 23:14:12 +0900

    Feat :: 회원가입 Request

---
+++ app/src/main/java/com/example/bank/RegisterRequest.java
@@ -0,0 +1,40 @@
+package com.example.bank;
+
+public class RegisterRequest {
+    String name;
+    String username;
+    String password;
+    String sec_password;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSec_password() {
+        return sec_password;
+    }
+
+    public void setSec_password(String sec_password) {
+        this.sec_password = sec_password;
+    }
+}
