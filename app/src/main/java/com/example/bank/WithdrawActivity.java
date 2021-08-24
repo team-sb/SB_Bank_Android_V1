e736c2a2997a0099437497c3fde2cad8c6782255
limsaehyun <sh00710000@gmail.com>
2021-08-24 22:55:36 +0900

    Feat :: 금액 입력 후 확인 기능

--- app/src/main/java/com/example/bank/WithdrawActivity.java
+++ app/src/main/java/com/example/bank/WithdrawActivity.java
@@ -1,14 +1,47 @@
 package com.example.bank;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WithdrawActivity extends AppCompatActivity {
 
+    ImageButton withDraw_ib_back;
+
+    TextView tv_createApoorovalNum;
+    TextView withdraw_tv_cancel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_withdraw);
+
+        withDraw_ib_back = (ImageButton) findViewById(R.id.withDraw_ib_back);
+        withDraw_ib_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        tv_createApoorovalNum = (TextView) findViewById(R.id.tv_createApoorovalNum);
+        tv_createApoorovalNum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WithdrawActivity.this, ApprovalNumberActivity.class));
+            }
+        });
+
+        withdraw_tv_cancel = (TextView) findViewById(R.id.withdraw_tv_cancel);
+        withdraw_tv_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
     }
 }
