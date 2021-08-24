9830b08eb8b35f02a04905a2e6642bd282dd0bb9
limsaehyun <sh00710000@gmail.com>
2021-08-24 19:04:08 +0900

    Feat :: 메인 페이지로 이동

--- app/src/main/java/com/example/bank/StartActivity.java
+++ app/src/main/java/com/example/bank/StartActivity.java
@@ -2,13 +2,27 @@ package com.example.bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class StartActivity extends AppCompatActivity {
+
+    ImageButton ib_startBanking;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        ib_startBanking = (ImageButton) findViewById(R.id.ib_startBanking);
+
+        ib_startBanking.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(StartActivity.this, MainActivity.class));
+            }
+        });
     }
 }
