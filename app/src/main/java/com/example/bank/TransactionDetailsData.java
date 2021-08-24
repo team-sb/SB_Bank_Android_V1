05656ea12c4e603afdfc7fdc856333051224c6a4
limsaehyun <sh00710000@gmail.com>
2021-08-24 23:25:58 +0900

    Feat :: 거래 내역 조회를 위한 데이터 클래스

---
+++ app/src/main/java/com/example/bank/TransactionDetailsData.java
@@ -0,0 +1,17 @@
+package com.example.bank;
+
+import com.google.gson.JsonObject;
+
+import java.util.List;
+
+public class TransactionDetailsData {
+    List<JsonObject> transactions;
+
+    public List<JsonObject> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<JsonObject> transactions) {
+        this.transactions = transactions;
+    }
+}
