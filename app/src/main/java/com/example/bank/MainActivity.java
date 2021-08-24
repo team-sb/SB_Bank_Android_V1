d09f96d7e2a74d5d4720aafc5644986844bc1f97
limsaehyun <sh00710000@gmail.com>
2021-08-24 19:03:58 +0900

    Feat :: 잔액&계좌 생성&QR체크인 클릭 이벤트

--- app/src/main/java/com/example/bank/MainActivity.java
+++ app/src/main/java/com/example/bank/MainActivity.java
@@ -1,14 +1,67 @@
 package com.example.bank;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_price;
+    TextView tv_makeAccount;
+    TextView tv_qrCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_price = (TextView) findViewById(R.id.tv_price);
+        tv_price.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TransactionDetailsActivity.class));
+            }
+        });
+
+        tv_makeAccount = (TextView) findViewById(R.id.tv_makeAccount);
+        tv_makeAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("계좌를 생성하시겠습니까?").setMessage("계속하시려면 \"Create\"를 클릭해주세요.");
+
+                builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(MainActivity.this, CreateAccountActivity.class));
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
+
+        tv_qrCheck = (TextView) findViewById(R.id.tv_qrCheck);
+        tv_qrCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://nid.naver.com/login/qrCheckIn")));
+            }
+        });
     }
 }
