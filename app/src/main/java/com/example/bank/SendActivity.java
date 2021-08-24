1491f182d4e3854c1523b56e31d3e4edff360927
limsaehyun <sh00710000@gmail.com>
2021-08-24 22:51:23 +0900

    Feat :: 입금 시 입력 확인

--- app/src/main/java/com/example/bank/SendActivity.java
+++ app/src/main/java/com/example/bank/SendActivity.java
@@ -3,12 +3,50 @@ package com.example.bank;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Objects;
 
 public class SendActivity extends AppCompatActivity {
+
+    ImageButton send_ib_back;
+
+    ImageButton send_ib_sendMoney;
+
+    EditText send_et_money;
+
+    String money;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send);
+
+        send_et_money = (EditText) findViewById(R.id.send_et_money);
+
+        send_ib_back = (ImageButton) findViewById(R.id.send_ib_back);
+        send_ib_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        send_ib_sendMoney = (ImageButton) findViewById(R.id.send_ib_sendMoney);
+        send_ib_sendMoney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = send_et_money.getText().toString();
+                if(!(Objects.equals(money, "") || money == null)) {
+                    sendMoney();
+                }
+            }
+        });
+    }
+
+    private void sendMoney() {
     }
 }
