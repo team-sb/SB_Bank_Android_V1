ac606f089ca916ca8af6cd84c9d1ca19bd2b616e
limsaehyun <sh00710000@gmail.com>
2021-08-25 15:25:10 +0900

    Feat :: 2차 비밀번호 Response

---
+++ app/src/main/java/com/example/bank/SecPasswordResponse.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+public class SecPasswordResponse {
+    private String secToken;
+
+    public String getSecToken() {
+        return secToken;
+    }
+
+    public void setSecToken(String secToken) {
+        this.secToken = secToken;
+    }
+}
