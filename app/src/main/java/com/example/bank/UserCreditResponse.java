29719b1add59e86056ecf40e06af4d244c74fc4b
limsaehyun <sh00710000@gmail.com>
2021-08-25 16:44:38 +0900

    Feat :: 유저 신용등급 Response

---
+++ app/src/main/java/com/example/bank/UserCreditResponse.java
@@ -0,0 +1,13 @@
+package com.example.bank;
+
+public class UserCreditResponse {
+    String creditRating;
+
+    public String getCreditRating() {
+        return creditRating;
+    }
+
+    public void setCreditRating(String creditRating) {
+        this.creditRating = creditRating;
+    }
+}
