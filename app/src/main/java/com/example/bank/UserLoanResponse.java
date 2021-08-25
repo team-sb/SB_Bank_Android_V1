d3be44dc8c2921f3c76400b6e590c707ed313718
limsaehyun <sh00710000@gmail.com>
2021-08-25 16:44:51 +0900

    Feat :: 유저 대출 Response

---
+++ app/src/main/java/com/example/bank/UserLoanResponse.java
@@ -0,0 +1,22 @@
+package com.example.bank;
+
+public class UserLoanResponse {
+    String count;
+    String money;
+
+    public String getCount() {
+        return count;
+    }
+
+    public void setCount(String count) {
+        this.count = count;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+}
