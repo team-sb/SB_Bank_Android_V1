d97f1bbccf7972488e72c1a5e41de22637fced2f
limsaehyun <sh00710000@gmail.com>
2021-08-25 18:47:57 +0900

    Feat :: 이체할 대상 정보 받아오기

--- app/src/main/java/com/example/bank/SendDataActivity.java
+++ app/src/main/java/com/example/bank/SendDataActivity.java
@@ -2,13 +2,19 @@ package com.example.bank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SendDataActivity extends AppCompatActivity {
 
@@ -18,12 +24,15 @@ public class SendDataActivity extends AppCompatActivity {
 
     EditText et_accountNum;
 
-    String accountNum;
+    public static String accountNum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_data);
+
+        UserData.temp_token = UserData.user_token;
 
         et_accountNum = (EditText) findViewById(R.id.et_accountNum);
 
@@ -49,5 +58,41 @@ public class SendDataActivity extends AppCompatActivity {
     }
 
     private void depositSend() {
+
+        ServerAPI serverAPI = ApiProvider.getInstance().create(ServerAPI.class);
+
+        Call<Void> call = serverAPI.chargeAccount(UserData.temp_token, accountNum);
+
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+                int result = response.code();
+
+                if(result == 201) { // 성공
+                    UserData.temp_token = UserData.user_token;
+                } else if (result == 403) { // 2차 인증
+                    secCertified();
+                } else if (result == 401) {
+                    Toast.makeText(SendDataActivity.this, "Token이 유효하지 않습니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+
+            }
+        });
+
+    }
+
+    private void secCertified() {
+        startActivity(new Intent(SendDataActivity.this, SecPasswordActiviity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        depositSend();
     }
 }
