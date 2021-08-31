c4f1e632b39cd8ab29a29168c94c94c0fd84cb96
limsaehyun <sh00710000@gmail.com>
2021-08-31 19:51:31 +0900

    Feat :: 설정 버튼 추가

--- app/src/main/java/com/example/bank/User/activity/MyPageActivity.java
+++ app/src/main/java/com/example/bank/User/activity/MyPageActivity.java
@@ -11,12 +11,14 @@ import com.example.bank.R;
 public class MyPageActivity extends AppCompatActivity {
 
     ImageButton myPage_ib_back;
+    ImageButton ib_settings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_page);
 
+        ib_settings = (ImageButton) findViewById(R.id.ib_settings);
         myPage_ib_back = (ImageButton) findViewById(R.id.myPage_ib_back);
         myPage_ib_back.setOnClickListener(new View.OnClickListener() {
             @Override
