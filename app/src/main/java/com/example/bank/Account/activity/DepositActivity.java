915bfd9f0e33d6e43c021dff14a2be7dfca5a3a2
limsaehyun <sh00710000@gmail.com>
2021-09-01 09:44:49 +0900

    Feat :: 입금 기능 구현

--- app/src/main/java/com/example/bank/Account/activity/DepositActivity.java
+++ app/src/main/java/com/example/bank/Account/activity/DepositActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -16,11 +17,14 @@ import com.example.bank.R;
 import com.example.bank.ServerAPI;
 import com.example.bank.UserData;
 
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class DepositActivity extends AppCompatActivity {
 
+    private static final String TAG = "DepositActivity";
+    
     ImageButton deposit_ib_back;
 
     TextView tv_deposit;
@@ -51,7 +55,7 @@ public class DepositActivity extends AppCompatActivity {
             public void onClick(View v) {
                 depositMoney = Integer.parseInt(deposit_et_money.getText().toString());
                 if(!(depositMoney == 0)) {
-                    deposit();
+                    secCertified();
                 }
             }
         });
@@ -65,36 +69,6 @@ public class DepositActivity extends AppCompatActivity {
         });
     }
 
-    private void deposit() {
-
-        ServerAPI serverAPI = ApiProvider.getInstance().create(ServerAPI.class);
-
-        retrofit2.Call<Void> call = serverAPI.chargeAccount(UserData.temp_token, depositMoney);
-
-        call.enqueue(new Callback<Void>() {
-            @Override
-            public void onResponse(retrofit2.Call<Void> call, Response<Void> response) {
-                int result = response.code();
-
-                if(result == 201) { // 성공
-                    UserData.temp_token = UserData.user_token;
-                    Toast.makeText(DepositActivity.this, depositMoney + "원 입금", Toast.LENGTH_SHORT).show();
-                    finish();
-                } else if (result == 403) { // 2차 인증
-                    secCertified();
-                } else if (result == 401) {
-                    Toast.makeText(DepositActivity.this, "2차 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(retrofit2.Call<Void> call, Throwable t) {
-
-            }
-        });
-
-    }
-
     private void secCertified() {
         startActivity(new Intent(DepositActivity.this, SecPasswordActiviity.class));
     }
@@ -103,6 +77,35 @@ public class DepositActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        deposit();
+        if(SecPasswordActiviity.secSuccess) {
+            ServerAPI serverAPI = ApiProvider.getInstance().create(ServerAPI.class);
+
+            String bearerUserToken = "Bearer " + UserData.temp_token;
+
+            Call<Void> call = serverAPI.chargeAccount(bearerUserToken, depositMoney);
+
+            call.enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> call, Response<Void> response) {
+                    int result = response.code();
+
+                    if(result == 200) { // 성공
+                        UserData.temp_token = UserData.user_token;
+                        SecPasswordActiviity.secSuccess = false;
+
+                        Toast.makeText(DepositActivity.this, depositMoney + "원 입금", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else if (result == 403) {
+                        Toast.makeText(DepositActivity.this, "2차 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<Void> call, Throwable t) {
+                }
+            });
+
+        }
     }
 }
