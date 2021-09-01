c563d5cef3f70b41ce9f30e36db8ef52eaeaa47b
limsaehyun <sh00710000@gmail.com>
2021-09-01 09:45:03 +0900

    Feat :: 출금 기능 구현

--- app/src/main/java/com/example/bank/Account/activity/WithdrawActivity.java
+++ app/src/main/java/com/example/bank/Account/activity/WithdrawActivity.java
@@ -1,15 +1,25 @@
 package com.example.bank.Account.activity;
 
 import android.content.Intent;
+import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bank.ApiProvider;
+import com.example.bank.Auth.activity.SecPasswordActiviity;
 import com.example.bank.R;
+import com.example.bank.ServerAPI;
+import com.example.bank.UserData;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class WithdrawActivity extends AppCompatActivity {
 
@@ -42,7 +52,7 @@ public class WithdrawActivity extends AppCompatActivity {
             public void onClick(View v) {
                 withdrawMoney = Integer.parseInt(withdraw_et_money.getText().toString()) * -1;
                 if(!(withdrawMoney == 0)) {
-                    startActivity(new Intent(WithdrawActivity.this, ApprovalNumberActivity.class));
+                    startActivity(new Intent(WithdrawActivity.this, SecPasswordActiviity.class));
                 }
             }
         });
@@ -54,5 +64,43 @@ public class WithdrawActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if(SecPasswordActiviity.secSuccess) {
+            ServerAPI serverAPI = ApiProvider.getInstance().create(ServerAPI.class);
+
+            String bearerUserToken = "Bearer " + UserData.temp_token;
+
+            Call<Void> call = serverAPI.chargeAccount(bearerUserToken, withdrawMoney);
+
+            call.enqueue(new Callback<Void>() {
+                @Override
+                public void onResponse(Call<Void> call, Response<Void> response) {
+                    int result = response.code();
+
+                    if(result == 200) { // 성공
+                        UserData.temp_token = UserData.user_token;
+                        SecPasswordActiviity.secSuccess = false;
+
+                        finish();
+                        startActivity(new Intent(WithdrawActivity.this, ApprovalNumberActivity.class));
+                    } else if (result == 403) {
+                        Toast.makeText(WithdrawActivity.this, "2차 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    } else if (result == 404) {
+                        Toast.makeText(WithdrawActivity.this, "잔액이 부족합니다.", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<Void> call, Throwable t) {
+                }
+            });
+
+        }
     }
 }
