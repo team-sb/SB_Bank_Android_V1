e757b0b24165cafc162562fc530f234e53143b41
limsaehyun <sh00710000@gmail.com>
2021-09-02 13:35:50 +0900

    Feat :: 대출 리사이클러뷰 어댑터

---
+++ app/src/main/java/com/example/bank/LoanListAdapter.java
@@ -0,0 +1,63 @@
+package com.example.bank;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class LoanListAdapter extends RecyclerView.Adapter<LoanListAdapter.CustomViewHolder>{
+
+    Context Ctx;
+    ArrayList<LoanListData> arraylist;
+
+    public LoanListAdapter(ArrayList<LoanListData> arraylist, Context Ctx) {
+        this.arraylist = arraylist;
+        this.Ctx = Ctx;
+    }
+
+    @NonNull
+    @Override
+    public LoanListAdapter.CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.loan_view, parent, false);
+        CustomViewHolder holder = new CustomViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LoanListAdapter.CustomViewHolder holder, int position) {
+        holder.view_tv_borrowedDate.setText("" + arraylist.get(position).getBorrowedDate());
+        holder.view_tv_money.setText("" + arraylist.get(position).getMoney());
+        holder.view_tv_interest.setText("" + arraylist.get(position).getInterest());
+        holder.view_tv_loanExpirationDate.setText("" + arraylist.get(position).getLoanExpirationDate());
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != arraylist ? arraylist.size() : 0);
+    }
+
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
+
+        protected TextView view_tv_borrowedDate;
+        protected TextView view_tv_money;
+        protected TextView view_tv_interest;
+        protected TextView view_tv_loanExpirationDate;
+
+        public CustomViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.view_tv_borrowedDate = (TextView) itemView.findViewById(R.id.view_tv_borrowedDate);
+            this.view_tv_money = (TextView) itemView.findViewById(R.id.view_tv_money);
+            this.view_tv_interest = (TextView) itemView.findViewById(R.id.view_tv_interest);
+            this.view_tv_loanExpirationDate = (TextView) itemView.findViewById(R.id.view_tv_loanExpirationDate);
+        }
+    }
+}
