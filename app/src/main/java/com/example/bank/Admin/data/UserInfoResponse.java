3b9c5690fd1690a406cfe8f008337734fa74517b
limsaehyun <sh00710000@gmail.com>
2021-09-03 22:28:58 +0900

    Feat :: 유저 정보 계좌 조회

---
+++ app/src/main/java/com/example/bank/Admin/data/UserInfoResponse.java
@@ -0,0 +1,31 @@
+package com.example.bank.Admin.data;
+
+public class UserInfoResponse {
+    String name;
+    String accountNumber;
+    String balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getBalance() {
+        return balance;
+    }
+
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+}
